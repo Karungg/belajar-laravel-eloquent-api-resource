576d64457703473eec47cd3ee718a7263d951ac0
Karungg <miftahfadilah71@gmail.com>
2023-11-19 20:20:17 +0700

    add relationship category has many products

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $table = "categories";
+    protected $primaryKey = "id";
+    protected $keyType = "int";
+    public $incrementing = true;
+    public $timestamp = true;
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class, "category_id", "id");
+    }
 }
